3ef7cfb43e6e32e1797a765c38b1a358c5fb12c2
Soufiane El ouezgany <ouzgani2006@gmail.com>
2026-09-14 06:13:53 +0100

    feat(main): run simulation and display results

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 from fly_in import parse_map
 from fly_in import find_path
+from fly_in import models
+from fly_in import display
 
 
 
@@ -19,11 +21,32 @@ def main():
     with open(map_file, "r") as file:
         data = parse_map(file)
 
-    find_path(data["graph"], data["graph"].start, data["graph"].end)
+    path = find_path(data["graph"], data["graph"].start, data["graph"].end)
     
+    if not path:
+        print("No path exists between start and end")
+        exit(1)
     
+    drones = [models.Drone(f"D{i+1}") for i in range(data["nb_drones"])]
+
+    for drone in drones:
+        drone.path = path
+        
+    simulation = models.Simulation(data["graph"], drones)
+    # simulation.run()
+    
+    # for turn_moves in simulation.log:
+    #     if turn_moves:
+    #         print(" ".join(turn_moves))
+
+    # print(f"\nCompleted in {simulation.turn} turns.")
+    while not simulation.all_delivered():
+        simulation.run_turn()
+        display.print_turn(data["graph"], drones, path, simulation.turn)
+
+    print(f"\nCompleted in {simulation.turn} turns.")
+
     return data   
 if __name__ == "__main__":
-    # print(main())
     main()
     
